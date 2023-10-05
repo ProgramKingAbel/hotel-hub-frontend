214f5478a36a344ec34f921dc2e21fece3c8d6b2
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-05 22:14:43 +0300

    Configure tailwind and material-tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-tailwind/react';
 
 function App() {
   return (
     <div>
-      <h1>Hello, React</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Button</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@material-tailwind/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
