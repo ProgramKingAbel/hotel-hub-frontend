b5ff1b870b7b0496f2eaa6d06c60e200709e8d28
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-09 13:34:12 +0300

    Create navbar as shared layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    { path: '/', text: 'Sign Up' },
+    { path: 'Rooms', text: 'Rooms' },
+    { path: 'Reservations', text: 'Reserve a Room' },
+    { path: 'Profile', text: 'My Reservations' },
+  ];
+
+  const Navbar = () => (
+    <nav>
+      <h1>Hotel Hub</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+
+export default Navbar
