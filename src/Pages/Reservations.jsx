ca0408131d5d190c3b43efe72035ae0a7c5dcd80
Enoisong <enobongitiaba988@gmail.com>
2023-10-10 05:19:38 -0700

    Add room reservation components

--- src/Pages/Reservations.jsx
+++ src/Pages/Reservations.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchReservationDetailsById, fetchReservations } from '../redux/features/reservations/reservationsSlice';
- 
+import { fetchReservationDetailsById, fetchReservations } from '../redux/features/reservations/reservationsSlice'; 
 
 const Reservation = () => {
   const dispatch = useDispatch();
