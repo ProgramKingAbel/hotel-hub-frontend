4c1b0bbbd36a563df414eb6baf983c171ed0d081
Waweru <ngashalex8@gmail.com>
2023-10-11 17:16:07 +0300

    worked on humbeger menu on mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
 
       <IconButton
         variant="text"
-        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+        className="ml-auto h-6 w-6 text-inherit text-black hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
         ripple={false}
         onClick={toggleMobileNav}
       >
