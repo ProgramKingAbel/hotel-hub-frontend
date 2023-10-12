75015cb6326cc3ac523e4bf09b537b883edde4b1
Peter Dumbari <dummytwiz@gmail.com>
2023-10-12 11:52:38 +0100

    added table to room detail

--- src/Pages/RoomItem.jsx
+++ src/Pages/RoomItem.jsx
@@ -1,37 +1,65 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchRoomDetailsById } from "../redux/features/rooms/roomsSlice";
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Typography } from '@material-tailwind/react';
+import { fetchRoomDetailsById } from '../redux/features/rooms/roomsSlice';
+import Table from '../components/Table/Table';
 
 const RoomItem = () => {
   const { roomId } = useParams();
   const dispatch = useDispatch();
   const roomDetails = useSelector((state) => state.room.roomDetails);
 
+  const TABLE_HEAD = ['Available Free packages'];
+
+  const TABLE_ROWS = [
+    {
+      name: 'Free Wifi',
+    },
+    {
+      name: 'Free Breakfast',
+    },
+    {
+      name: "65'HDTV",
+    },
+    {
+      name: 'Air Conditioning',
+    },
+  ];
+
   useEffect(() => {
     dispatch(fetchRoomDetailsById(roomId));
   }, [dispatch, roomId]);
 
   return (
-    <div className="container mx-auto max-w-screen-xl lg:pl-40 w-full room_container mt-40 bg-slate-900">
+    <div className="container mx-auto max-w-screen-xl lg:pl-40 w-full room_container mt-20 bg-slate-900">
       <div
-        className="container flex flex-col sm:flex-col md:flex-col lg:flex-row justify-between w-full"
-        key={roomDetails.name}>
-        <div className="flex-1 lg:flex-grow-2 image_container ">
+        className="container flex flex-col sm:flex-col md:flex-col lg:flex-row justify-between texts-center lg:gap-20 w-full"
+        key={roomDetails.name}
+      >
+        <div className="flex-9 lg:flex-grow-9 image_container">
           <img
             src={roomDetails && roomDetails.image}
             alt={roomDetails.name}
-            className="w-full object-cover object-center sm:w-400 sm:h-800"
+            className="h-96 w-full object-cover object-center sm:w-400 sm:h-800"
           />
-
           <div className="mt-6">
             <button type="button">More Image</button>
           </div>
         </div>
-        <div className="flex flex-col justify-center flex-1 lg:flex-grow test_coting ">
-          <h1>{roomDetails && roomDetails.name}</h1>
-          <p>{roomDetails && roomDetails.description}</p>
+        <div className="flex-1 lg:flex-grow text_container sm:w-1/5 lg:w-full">
+          <Typography variant="h3">
+            {roomDetails && roomDetails.name}
+          </Typography>
+          <p className="desc">{roomDetails && roomDetails.description}</p>
+          <p className="price">
+            $
+            {roomDetails && roomDetails.price}
+          </p>
 
+          <div className="mt-6">
+            <Table tablehead={TABLE_HEAD} tablerows={TABLE_ROWS} />
+          </div>
           <div className="flex items-center justify-center mt-6">
             <button type="button">Reserve this room</button>
           </div>
