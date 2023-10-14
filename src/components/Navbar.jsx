9072006d1f29634c2db534ab65028b6d68e43d75
Peter Dumbari <dummytwiz@gmail.com>
2023-10-15 00:35:15 +0100

    implemeted auto menu close

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-import '../styles/components/navbar.scss';
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import "../styles/components/navbar.scss";
 import {
   Typography,
   List,
@@ -8,36 +8,36 @@ import {
   Card,
   MobileNav,
   IconButton,
-} from '@material-tailwind/react';
+} from "@material-tailwind/react";
 import {
   HomeIcon,
   ShoppingBagIcon,
   UserCircleIcon,
   PowerIcon,
-} from '@heroicons/react/24/solid';
+} from "@heroicons/react/24/solid";
 
 const links = [
   {
-    path: '/app',
-    text: 'Rooms',
+    path: "/app",
+    text: "Rooms",
     icon: HomeIcon,
     exact: true,
   },
   {
-    path: '/app/Reservations',
-    text: 'Reserve a Room',
+    path: "/app/Reservations",
+    text: "Reserve a Room",
     icon: ShoppingBagIcon,
     exact: false,
   },
   {
-    path: '/app/Profile',
-    text: 'My Reservations',
+    path: "/app/Profile",
+    text: "My Reservations",
     icon: UserCircleIcon,
     exact: false,
   },
   {
-    path: '/',
-    text: 'Sign Out',
+    path: "/",
+    text: "Sign Out",
     icon: PowerIcon,
     exact: false,
   },
@@ -47,6 +47,18 @@ const Navbar = () => {
   const { pathname } = useLocation();
   const [openMobileNav, setOpenMobileNav] = useState(false);
   const [activeLink, setActiveLink] = useState(pathname);
+  const screen = document.body;
+
+  document.addEventListener("click", () => {
+    screen.addEventListener("click", (event) => {
+      if (openMobileNav && screen.contains(event.target)) {
+        const togglerButton = document.querySelector(".toggle_icon");
+        if (!togglerButton.contains(event.target)) {
+          setOpenMobileNav(false);
+        }
+      }
+    });
+  });
 
   const handleNavLinkClick = (path) => {
     setActiveLink(path);
@@ -69,17 +81,16 @@ const Navbar = () => {
         <List>
           {links.map((link) => (
             <ListItem key={link.text} className="mb-2 ml-4 list">
-              {pathname !== '/' ? (
+              {pathname !== "/" ? (
                 <NavLink
                   exact={link.exact}
                   to={link.path}
                   className={`flex items-center p-2 text-lg rounded-none ${
-                    link.path === activeLink ? 'bg-blue-500' : ''
+                    link.path === activeLink ? "bg-blue-500" : ""
                   }`}
-                  onClick={() => handleNavLinkClick(link.path)}
-                >
+                  onClick={() => handleNavLinkClick(link.path)}>
                   {React.createElement(link.icon, {
-                    className: 'h-5 w-5 mr-2',
+                    className: "h-5 w-5 mr-2",
                   })}
                   {link.text}
                 </NavLink>
@@ -95,8 +106,7 @@ const Navbar = () => {
         variant="text"
         className="ml-auto h-6 w-6 text-inherit text-black hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden toggle_icon absolute top-4 right-4"
         ripple={false}
-        onClick={toggleMobileNav}
-      >
+        onClick={toggleMobileNav}>
         {openMobileNav ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -104,7 +114,7 @@ const Navbar = () => {
             viewBox="0 0 24 24"
             stroke="currentColor"
             strokeWidth={2}
-          >
+            className="toggler_icon">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -116,8 +126,7 @@ const Navbar = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
-            strokeWidth={2}
-          >
+            strokeWidth={2}>
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -132,23 +141,21 @@ const Navbar = () => {
         onClose={() => setOpenMobileNav(false)}
         className="h-screen w-50 p-4 gap-8 md:flex rounded-none absolute top-0 left-0 mobile_nav"
         style={{
-          backgroundColor: 'rgba(100, 100, 100, 0.9)',
+          backgroundColor: "rgba(100, 100, 100, 0.9)",
           zIndex: 1000,
-        }}
-      >
+        }}>
         <List>
           {links.map((link) => (
             <ListItem key={link.text}>
-              {pathname !== '/' ? (
+              {pathname !== "/" ? (
                 <NavLink
                   to={link.path}
                   className={`flex items-center items${
-                    link.path === activeLink ? 'active' : ''
+                    link.path === activeLink ? "active" : ""
                   }`}
-                  onClick={() => handleNavLinkClick(link.path)}
-                >
+                  onClick={() => handleNavLinkClick(link.path)}>
                   {React.createElement(link.icon, {
-                    className: 'h-5 w-5 mr-2',
+                    className: "h-5 w-5 mr-2",
                   })}
                   {link.text}
                 </NavLink>
