d70d7f5023bd4b2c0a691a95b1f45eace4e7eeaa
Peter Dumbari <dummytwiz@gmail.com>
2023-10-16 00:38:59 +0100

    working on the styles

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
-import { fetchRooms } from '../redux/features/rooms/roomsSlice';
-import { createReservation } from '../redux/features/reservations/reservationsSlice';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState, useEffect } from "react";
+import { Card, Input, Select, Option, Button } from "@material-tailwind/react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import { fetchRooms } from "../redux/features/rooms/roomsSlice";
+import { createReservation } from "../redux/features/reservations/reservationsSlice";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ReservationForm = () => {
   const dispatch = useDispatch();
   const rooms = useSelector((state) => state.room.rooms);
-  const storedUserData = localStorage.getItem('userData');
+  const storedUserData = localStorage.getItem("userData");
   const currentUser = JSON.parse(storedUserData);
   const navigate = useNavigate();
   console.log(currentUser.name);
@@ -17,7 +18,7 @@ const ReservationForm = () => {
   console.log(roomId);
   const [formData, setFormData] = useState({
     username: currentUser.name,
-    room: roomId || '',
+    room: roomId || "",
     check_in: new Date(),
     check_out: new Date(),
   });
@@ -49,61 +50,66 @@ const ReservationForm = () => {
     };
     try {
       await dispatch(createReservation(requestData));
-      navigate('/app/Profile');
+      navigate("/app/Profile");
     } catch (error) {
-      console.error('Error adding room:', error);
-      setError('An error occurred. Please try again.');
+      console.error("Error adding room:", error);
+      setError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <div>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-            disabled
-          />
-        </div>
-        <div>
-          <label>Room:</label>
-          <select
-            name="room"
-            value={formData.room}
-            onChange={handleInputChange}
-            disabled={!!roomId}
-          >
-            <option value="">Select a room</option>
-            {rooms.map((room) => (
-              <option key={room.id} value={room.id}>
-                {room.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Check-In Date:</label>
-          <DatePicker
-            selected={formData.check_in}
-            name="check_in"
-            onChange={(date) => handleDateChange(date, 'check_in')}
-          />
-        </div>
-        <div>
-          <label>Check-Out Date:</label>
-          <DatePicker
-            selected={formData.check_out}
-            name="check_out"
-            onChange={(date) => handleDateChange(date, 'check_out')}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="md:container w-full reservation_form_container">
+      <div className="inner_reservation">
+        {error && <p className="error-message">{error}</p>}
+
+        <Card color="transparent" shadow={false}>
+          <form
+            className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+            onSubmit={handleSubmit}>
+            <div className="mb-4 flex flex-col gap-6">
+              <Input
+                size="lg"
+                label="Username"
+                value={formData.username}
+                onChange={handleInputChange}
+                disabled
+              />
+              <div className="mt-3">
+                <label>Check-Out Date:</label>
+                <Select label="Room" disabled={!!roomId}>
+                  {rooms.map((room) => (
+                    <Option key={room.id} value={room.id}>
+                      {room.name}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <div>
+                <label>Check-In Date:</label>
+                <DatePicker
+                  selected={formData.check_in}
+                  name="check_in"
+                  onChange={(date) => handleDateChange(date, "check_in")}
+                  className="w-full border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+                />
+              </div>
+              <div>
+                <label>Check-Out Date:</label>
+                <DatePicker
+                  selected={formData.check_out}
+                  name="check_out"
+                  onChange={(date) => handleDateChange(date, "check_out")}
+                  className="w-full border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
+                />
+              </div>
+            </div>
+
+            <Button className="mt-6" fullWidth>
+              Submit
+            </Button>
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
