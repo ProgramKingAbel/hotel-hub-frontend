4c60a7b1d9903f30290d680a4bc0e8bc8ae7643d
sahlminkok <sahlminkok7@gmail.com>
2023-10-18 10:47:29 +0100

    Add h-screen class to loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Loader.scss';
 
 const Loader = () => (
-  <div className="loader_container">
+  <div className="loader_container h-screen">
     <div className="loader" />
   </div>
 );
